35a4a4b3271d8b9b34c57b220aae387d3fbff46e
Madi Ostoja <madi@madeleineostoja.com>
2026-05-28 23:28:51 +1200

    nit: isolate false on vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    isolate: false,
     projects: ["packages/*"],
     environment: "node",
     globals: false,
